fd962d5d36cfb105f04423b2cc45acf146017f7d
ndokas99 <ndokaskuda1999@gmail.com>
2025-06-24 04:48:13 +0200

    Fix v1

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from main.views import PlanRouteView
 
 urlpatterns = [
-    path('route-api', PlanRouteView.as_view(), name='route-api')
+    path('route-api', PlanRouteView.as_view(), name='route-api/')
 ]
